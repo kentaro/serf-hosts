73b1e027a0ee2b12c84da1ec67e1fbf5676cf0ea
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-30 01:36:04 +0900

    More appropriate file update

--- event_handler.pl
+++ event_handler.pl
@@ -1,31 +1,47 @@
 #!/usr/bin/env perl
+
 use strict;
 use warnings;
+use Fcntl qw(:flock);
+use File::Temp qw(tempfile);
+use File::Copy;
 
+my $file = $ARGV[0] || 'etc/hosts';
 my ($name, $address, $role) = split(/\s+/, <STDIN>);
 my $event = $ENV{SERF_EVENT};
 
 if ($event eq 'member-join') {
-    open my $fh, ">> etc/hosts" or die $!;
-    print $fh "${address}\t${name}\n";
+    open my $fh, ">> ${file}" or die $!;
+
+    {
+        flock($fh, LOCK_EX);
+        print $fh "${address}\t${name}\n";
+        flock($fh, LOCK_UN);
+    }
+
     close $fh;
 }
 elsif (
     $event eq 'member-leave' ||
     $event eq 'member-failed'
 ) {
-    my $content = '';
+    open my $fh, "< ${file}" or die $!;
+    my ($tmp_fh, $tmp_file) = tempfile();
 
-    open my $fh, "< etc/hosts" or die $!;
-    while (<$fh>) {
-        if ($_ !~ /${name}$/) {
-            $content .= $_;
+    {
+        flock($fh, LOCK_EX);
+        while (<$fh>) {
+            if ($_ !~ /${name}$/) {
+                print $tmp_fh $_;
+            }
         }
+        flock($fh, LOCK_UN);
     }
+
     close $fh;
 
-    open  $fh, "> etc/hosts" or die $!;
-    print $fh $content;
-    close $fh;
+    File::Copy::move($tmp_file, $file) or
+        die "Failed to move ${tmp_file} to ${file}";
+    close $tmp_fh;
 }
 
